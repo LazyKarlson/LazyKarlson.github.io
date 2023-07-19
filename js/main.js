52793c021766b29435e2e7ee405ddb17345fb848
Yury <yurha@protonmail.com>
2023-07-19 12:21:55 +0300

    Hungary

--- js/main.js
+++ js/main.js
@@ -111,7 +111,7 @@ function goBack(event) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  var date = new Date('6/17/2023 20:00:00 UTC');
+  var date = new Date('7/22/2023 14:00:00 UTC');
   var countTo = date.getTime() / 1000;
 
   // Set up FlipDown
